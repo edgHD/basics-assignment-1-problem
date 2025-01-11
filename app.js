69d0f4b715a94ad7932b6a2e7a09475204e6254d
Edgar <138873391+edgHD@users.noreply.github.com>
2025-01-10 22:49:20 -0500

    Updated app.js and fixed app.html
    I made calculations to show my current age based on today's date.

--- app.js
+++ app.js
@@ -2,19 +2,31 @@ const app = Vue.createApp({
     data() {
         return {
             myName: 'Edgar',
-            myAge: 19,
+            birthYear: 2005,
+            currentYear: new Date().getFullYear(),
             imgURL: 'https://northfur.ca/345-large_default/dolphin-muzzle.jpg'
         }
     },
     computed: {
+        myAge() {
+            const currentMonth = new Date().getMonth(); // Months starts at index 0
+            const currentDay = new Date().getDate();
+            const BirthAge = this.currentYear - this.birthYear;
+
+            if (currentMonth < 4 || (currentMonth === 4 && currentDay  < 19)) {
+                return BirthAge - 1;
+            } else {
+                return BirthAge;
+            }
+        },
         meIn5Years() {
-            return this.myAge + 5
+            return this.myAge + 5;
         }
     },
     methods: {
         favNum() {
             const randomNum = Math.random();
-            return `Favourite Number: ${randomNum}`
+            return `Favourite Number: ${randomNum}`;
         }
     }
 });
